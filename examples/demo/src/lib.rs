027cc4d9ce3d1ee41b62c01ab82be290dee9f04b
leod <subtle.frustration@proton.me>
2021-07-18 12:19:48 +0200

    Add structure for example demo

---
+++ examples/demo/src/lib.rs
@@ -0,0 +1,9 @@
+use wasm_bindgen::prelude::wasm_bindgen;
+
+#[wasm_bindgen(start)]
+pub fn main() {
+    console_error_panic_hook::set_once();
+    console_log::init_with_level(log::Level::Debug).unwrap();
+
+    log::info!("Hi, starting the example");
+}
